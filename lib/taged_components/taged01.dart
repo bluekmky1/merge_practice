31812f9fe662db0fd5b8730acd201bce070ad848
bluekmky1 <115731932+bluekmky1@users.noreply.github.com>
2023-07-03 02:53:00 +0900

    💄 taged01 (#2) (#4)
    pr

---
+++ lib/taged_components/taged01.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Taged extends StatelessWidget {
+  const Taged({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
